b9d91eb0d894ece50362c08ebf22ed538bb9226c
Dnyaneshwari Suryawanshi <mi.dnyanai@gmail.com>
2025-01-16 15:58:35 +0530

    conversion and operations

---
+++ 03_ConversionOperations.js
@@ -0,0 +1,40 @@
+let score = "33abc"
+score = null
+score = undefined
+score = true
+score = "Pravin"
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN   Not a Number
+// true => 1;  false => 0
+
+
+let isLoggedIn = ""
+
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+//console.log(boolenIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Pravin" =>  true
+
+
+
+let someNumber = 33
+let stringNumer = String(someNumber)
+console.log(stringNumer);
+console.log(typeof stringNumer);
+
+
+//
+
